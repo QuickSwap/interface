8ea06c6e8df2cd3f1b3197ba4f21cffa733909ec
Sameep Singhania <sameepsi@gmail.com>
2020-09-30 16:55:46 +0530

    Latest build

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -19,7 +19,7 @@ export const network = new NetworkConnector({
 })
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42, 80001, 137]
+  supportedChainIds: [ 137, 80001]
 })
 
 // mainnet only
