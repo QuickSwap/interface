0a875e815a16a4dd510ee55517208165271e8382
Sameep Singhania <sameepsi@gmail.com>
2020-10-09 14:43:51 +0530

    updated default token list

--- build/static/js/7.80ae79d2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpquickswap-interface"]=this["webpackJsonpquickswap-interface"]||[]).push([[7],{1007:function(n,c){},926:function(n,c){},927:function(n,c){},953:function(n,c){},954:function(n,c){},960:function(n,c){}}]);
-//# sourceMappingURL=7.80ae79d2.chunk.js.map
