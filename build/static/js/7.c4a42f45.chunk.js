13d0aa558fc019928165dca255bc6e541fe743dd
sameepsi <sameepsi@gmail.com>
2021-07-21 18:30:13 +0530

    Latest build

--- build/static/js/7.c4a42f45.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpquickswap-interface"]=this["webpackJsonpquickswap-interface"]||[]).push([[7],{2011:function(c,i){}}]);
-//# sourceMappingURL=7.c4a42f45.chunk.js.map
--- src/pages/LimitOrder/index.tsx
+++ src/pages/LimitOrder/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { GelatoLimitOrderPanel, GelatoLimitOrdersHistoryPanel } from '@gelatonetwork/limit-orders-react'
+import { TYPE } from '../../theme'
 
 export default function LimitOrder() {
   return (
     <>
       <GelatoLimitOrderPanel />
       <GelatoLimitOrdersHistoryPanel />
+      <TYPE.main style={{ textAlign: 'center', marginTop: 20}} fontSize={16}>
+          Disclaimer: Limit orders are powered by Gelato, a third party service. Please use at your own risk.
+        </TYPE.main>
     </>
   )
 }
