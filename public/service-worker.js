79913a0a14cc6f1ce46b4347527c7054f827dc2a
sameepsi <sameepsi@gmail.com>
2022-09-01 17:35:46 +0530

    SW Fx

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,5 @@
-/// <reference lib="webworker" />
-/* eslint-disable no-restricted-globals */
-
-import 'workbox-precaching'
-
-declare const self: ServiceWorkerGlobalScope
-
 self.addEventListener('install', () => {
-  console.log("new service worker install")
+  console.log("Installin SW")
   // Skip over the "waiting" lifecycle state, to ensure that our
   // new service worker is activated immediately, even if there's
   // another tab open controlled by our older service worker code.
@@ -14,21 +7,22 @@ self.addEventListener('install', () => {
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-  console.log("new service worker activate")
-  self.registration.unregister().then(() => {
-    console.log('QUICKSWAP Safety Worker - unregistered old service worker');
-  });
-  
   // Optional: Get a list of all the current open windows/tabs under
   // our service worker's control, and force them to reload.
   // This can "unbreak" any open windows/tabs as soon as the new
   // service worker activates, rather than users having to manually reload.
+  console.log("Activating SW")
+  event.waitUntil(self.clients.claim());
+  
+  console.log("new service worker activate")
+  self.registration.unregister().then(() => {
+    console.log('QUICKSWAP Safety Worker - unregistered old service worker');
+  });
+
   self.clients.matchAll({
     type: 'window'
   }).then(windowClients => {
     windowClients.forEach((windowClient) => {
-      //@ts-ignore
       windowClient.navigate(windowClient.url);
     });
   });
